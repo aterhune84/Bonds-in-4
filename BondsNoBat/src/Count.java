0d7746553eeb3de34a6b30f31f3d25926f405044
aterhune84 <112529622+aterhune84@users.noreply.github.com>
2022-08-31 13:57:31 -0400

    Add files via upload

--- BondsNoBat/src/Count.java
+++ BondsNoBat/src/Count.java
@@ -4,260 +4,43 @@ public class Count {
     private int balls;
     private int strikes;
     private AtBatResult result;
-    private int balls00;
-    private int strikes00;
-    private int balls01;
-    private int strikes01;
-    private int balls02;
-    private int strikes02;
-    private int balls10;
-    private int strikes10;
-    private int balls11;
-    private int strikes11;
-    private int balls12;
-    private int strikes12;
-    private int balls20;
-    private int strikes20;
-    private int balls21;
-    private int strikes21;
-    private int balls22;
-    private int strikes22;
-    private int balls30;
-    private int strikes30;
-    private int balls31;
-    private int strikes31;
-    private int balls32;
-    private int strikes32;
     public Count(){
         balls = 0;
         strikes = 0;
         result = AtBatResult.Unknown;
-        balls00 = 0;
-        strikes00 = 0;
-        balls01 = 0;
-        strikes01 = 0;
-        balls02 = 0;
-        strikes02 = 0;
-        balls10 = 0;
-        strikes10 = 0;
-        balls11 = 0;
-        strikes11 = 0;
-        balls12 = 0;
-        strikes12 = 0;
-        balls20 = 0;
-        strikes20 = 0;
-        balls21 = 0;
-        strikes21 = 0;
-        balls22 = 0;
-        strikes22 = 0;
-        balls30 = 0;
-        strikes30 = 0;
-        balls31 = 0;
-        strikes31 = 0;
-        balls32 = 0;
-        strikes32 = 0;
     }
 
     public void nextPitch(char c){
         Random rand = new Random();
         if (c == 'B'){
-            if (balls == 0){
-                if (strikes == 0){
-                    balls00++;
-                }
-                else if (strikes == 1){
-                    balls01++;
-                }
-                else if (strikes == 2){
-                    balls02++;
-                }
-            }
-            else if (balls == 1){
-                if (strikes == 0){
-                    balls10++;
-                }
-                else if (strikes == 1){
-                    balls11++;
-                }
-                else if (strikes == 2){
-                    balls12++;
-                }
-            }
-            else if (balls == 2){
-                if (strikes == 0){
-                    balls20++;
-                }
-                else if (strikes == 1){
-                    balls21++;
-                }
-                else if (strikes == 2){
-                    balls22++;
-                }
-            }
-            else if (balls == 3){
-                if (strikes == 0){
-                    balls30++;
-                }
-                else if (strikes == 1){
-                    balls31++;
-                }
-                else if (strikes == 2){
-                    balls32++;
-                }
-            }
             balls++;
         }
         else if (c == 'I'){
             balls++;
         }
         else if (c == 'C'){
-            if (balls == 0){
-                if (strikes == 0){
-                    strikes00++;
-                }
-                else if (strikes == 1){
-                    strikes01++;
-                }
-                else if (strikes == 2){
-                    strikes02++;
-                }
-            }
-            else if (balls == 1){
-                if (strikes == 0){
-                    strikes10++;
-                }
-                else if (strikes == 1){
-                    strikes11++;
-                }
-                else if (strikes == 2){
-                    strikes12++;
-                }
-            }
-            else if (balls == 2){
-                if (strikes == 0){
-                    strikes20++;
-                }
-                else if (strikes == 1){
-                    strikes21++;
-                }
-                else if (strikes == 2){
-                    strikes22++;
-                }
-            }
-            else if (balls == 3){
-                if (strikes == 0){
-                    strikes30++;
-                }
-                else if (strikes == 1){
-                    strikes31++;
-                }
-                else if (strikes == 2){
-                    strikes32++;
-                }
-            }
             strikes++;
         }
-        else if (c == 'F' || c == 'L' || c == 'S' || c == 'T'){
+        else if (c == 'F' || c == 'L' || c == 'S' || c == 'T' || c == 'X'){
             if (rand.nextInt(999) < 191){
-                if (balls == 0){
-                    if (strikes == 0){
-                        balls00++;
-                    }
-                    else if (strikes == 1){
-                        balls01++;
-                    }
-                    else if (strikes == 2){
-                        balls02++;
-                    }
-                }
-                else if (balls == 1){
-                    if (strikes == 0){
-                        balls10++;
-                    }
-                    else if (strikes == 1){
-                        balls11++;
-                    }
-                    else if (strikes == 2){
-                        balls12++;
-                    }
-                }
-                else if (balls == 2){
-                    if (strikes == 0){
-                        balls20++;
-                    }
-                    else if (strikes == 1){
-                        balls21++;
-                    }
-                    else if (strikes == 2){
-                        balls22++;
-                    }
-                }
-                else if (balls == 3){
-                    if (strikes == 0){
-                        balls30++;
-                    }
-                    else if (strikes == 1){
-                        balls31++;
-                    }
-                    else if (strikes == 2){
-                        balls32++;
-                    }
-                }
                 balls++;
             }
             else{
-                if (balls == 0){
-                    if (strikes == 0){
-                        strikes00++;
-                    }
-                    else if (strikes == 1){
-                        strikes01++;
-                    }
-                    else if (strikes == 2){
-                        strikes02++;
-                    }
-                }
-                else if (balls == 1){
-                    if (strikes == 0){
-                        strikes10++;
-                    }
-                    else if (strikes == 1){
-                        strikes11++;
-                    }
-                    else if (strikes == 2){
-                        strikes12++;
-                    }
-                }
-                else if (balls == 2){
-                    if (strikes == 0){
-                        strikes20++;
-                    }
-                    else if (strikes == 1){
-                        strikes21++;
-                    }
-                    else if (strikes == 2){
-                        strikes22++;
-                    }
-                }
-                else if (balls == 3){
-                    if (strikes == 0){
-                        strikes30++;
-                    }
-                    else if (strikes == 1){
-                        strikes31++;
-                    }
-                    else if (strikes == 2){
-                        strikes32++;
-                    }
-                }
                 strikes++;
             }
         }
         else if (c == 'H'){
             result = AtBatResult.Walk;
         }
-        else if (c == 'X'){
-            result = AtBatResult.Hit;
+    }
+
+    public void createPitch(int balls, int strikes){
+        Random rand = new Random();
+        if (rand.nextInt(balls+strikes-1) < balls){
+            this.balls++;
+        }
+        else{
+            this.strikes++;
         }
     }
 
--- BondsNoBat/src/NoBat.java
+++ BondsNoBat/src/NoBat.java
@@ -1,19 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class NoBat{
     public static void main(String[] args) {
         if (args.length == 1) {
             String filename = args[0];
-            int takenBalls = 0, takenStrikes = 0, swung = 0;
-            int walks = 0, strikeouts = 0, hits = 0, total = 0;
+            int takenBalls = 0, takenStrikes = 0;
+            int walks = 0, strikeouts = 0, total = 0;
             Count count = new Count();
+            //Go through pitch data and generate balls and strikes
             try {
                 File myObj = new File(filename);
                 Scanner myReader = new Scanner(myObj);
-                while (myReader.hasNextLine()) {
-                    count.resetCount();
+                while (myReader.hasNextLine()){
                     String data = myReader.nextLine();
                     char[] chars = data.toCharArray();
                     for (char c: chars){
@@ -24,39 +25,87 @@ public class NoBat{
                             takenStrikes++;
                         }
                         else if (c == 'F' || c == 'L' || c == 'S' || c == 'T' || c == 'X'){
-                            swung++;
-                        }
-                        count.nextPitch(c);
-                        if (count.resultCheck() != AtBatResult.Unknown){
-                            AtBatResult result = count.resultCheck();
-                            if (result == AtBatResult.Walk){
-                                walks++;
-                                break;
+                            Random rand = new Random();
+                            if (rand.nextInt(999) < 191){
+                                takenBalls++;
                             }
-                            else if (result == AtBatResult.Strikeout){
-                                strikeouts++;
-                                break;
-                            }
-                            else if (result == AtBatResult.Hit){
-                                hits++;
-                                break;
+                            else{
+                                takenStrikes++;
                             }
                         }
                     }
-                    total++;
                 }
-                myReader.close();
-                System.out.println("Walks: " + walks);
-                System.out.println("Strikeouts: " + strikeouts);
-                System.out.println("Hits: " + hits);
-                System.out.println("Balls Taken: " + takenBalls);
-                System.out.println("Strikes Taken: " + takenStrikes);
-                System.out.println("Swung On: " + swung);
-                System.out.println("Total: " + total);
             } catch (FileNotFoundException e) {
-                System.out.println("An error occurred");
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
+            //Running the simulation 1000 times to produce more accurate numbers and account for variation
+            int totalWalks = 0;
+            int totalStrikeouts = 0;
+            int maxWalks = 0;
+            int maxStrikeouts = 0;
+            for (int i = 1; i <= 1000; i++){
+                walks = 0;
+                strikeouts = 0;
+                // Simulate each plate appearance without a bat
+                try {
+                    File myObj = new File(filename);
+                    Scanner myReader = new Scanner(myObj);
+                    while (myReader.hasNextLine()) {
+                        count.resetCount();
+                        String data = myReader.nextLine();
+                        char[] chars = data.toCharArray();
+                        for (char c: chars){
+                            count.nextPitch(c);
+                            if (count.resultCheck() != AtBatResult.Unknown){
+                                AtBatResult result = count.resultCheck();
+                                if (result == AtBatResult.Walk){
+                                    walks++;
+                                    break;
+                                }
+                                else if (result == AtBatResult.Strikeout){
+                                    strikeouts++;
+                                    break;
+                                }
+                            }
+                        }
+                        if (count.resultCheck() == AtBatResult.Unknown){
+                            while (count.resultCheck() == AtBatResult.Unknown){
+                                count.createPitch(takenBalls, takenStrikes);
+                            }
+                            if (count.resultCheck() == AtBatResult.Walk){
+                                walks++;
+                            }
+                            else if (count.resultCheck() == AtBatResult.Strikeout){
+                                strikeouts++;
+                            }
+                        }
+                        total++;
+                    }
+                    myReader.close();
+                    totalWalks += walks;
+                    totalStrikeouts += strikeouts;
+                    if (walks > maxWalks){
+                        maxWalks = walks;
+                    }
+                    if (strikeouts > maxStrikeouts){
+                        maxStrikeouts = strikeouts;
+                    }
+                } catch (FileNotFoundException e) {
+                    System.out.println("An error occurred");
+                    e.printStackTrace();
+                }
+            }
+            double averageWalks = totalWalks / 1000.0;
+            double averageStrikeouts = totalStrikeouts / 1000.0;
+            System.out.println("Taken Balls: " + takenBalls);
+            System.out.println("Taken Strikes: " + takenStrikes);
+            System.out.println("Average Walks: " + averageWalks);
+            System.out.println("Average Strikeouts: " + averageStrikeouts);
+            System.out.println("Most Walks: " + maxWalks);
+            System.out.println("Least Walks: " + (total / 1000 - maxStrikeouts));
+            System.out.println("Best OBP: " + maxWalks / ((double)total / 1000));
+            System.out.println("Worst OBP: " + (total / 1000 - maxStrikeouts) / ((double)total / 1000));
+            System.out.println("Average On Base Percentage: " + averageWalks / (averageWalks + averageStrikeouts));
         }
     }
 }
